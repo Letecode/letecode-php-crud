1061be7a44b7cbbe0f9dc6be6627229fb5ab317c
Jean Claude Mbiya <46892936+johnmbiya@users.noreply.github.com>
2022-12-06 22:26:33 +0100

    initial commit

---
+++ api/connection.php
@@ -0,0 +1,28 @@
+<?php
+
+    class Connection {
+
+        private $connection;
+
+        function __construct(){}
+
+        function connect() {
+            $this->connection = new mysqli('localhost', 'root', 'root', 'apprenants');
+
+            if(mysqli_connect_error()) {
+                echo 'Error';
+                die('connexion error'.mysqli_connect_error());
+            }
+
+            return $this->connection;
+        }
+    }
+
+
+
+
+
+
+
+
+?>
